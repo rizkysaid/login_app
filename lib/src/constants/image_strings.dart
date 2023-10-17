ed90f22c64340f39007520cfb718eb20c1c5a226
rizkysaidy <r.izky_s@yahoo.co.id>
2023-10-17 23:59:18 +0700

    perubahan folder splash_images

--- lib/src/constants/image_strings.dart
+++ lib/src/constants/image_strings.dart
@@ -1,3 +1,3 @@
-const String splashTopIcon = "assets/images/splash_top_icon.png";
-const String splashImage = "assets/images/rizkysaid_mask.jpeg";
+const String splashTopIcon = "assets/images/splash_images/splash_top_icon.png";
+const String splashImage = "assets/images/splash_images/rizkysaid_mask.jpeg";
 
